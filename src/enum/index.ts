1c5e8c6b5f169c2ff5b7e4a749a4213a563db9ed
atoz <523333404@qq.com>
2022-10-11 19:57:01 +0800

    枚举操作符

---
+++ src/enum/index.ts
@@ -0,0 +1,17 @@
+/**
+ * where查询条件 运算符 的映射关系
+ */
+export enum OP {
+    EQ           = '=',           // 等于
+    NEQ          = '<>',          // 不等于
+    GT           = '>',           // 大于
+    EGT          = '>=',          // 大于等于
+    LT           = '<',           // 小于
+    ELT          = '<=',          // 小于等于
+    LIKE         = 'LIKE',        // 模糊查询
+    BETWEEN      = 'BETWEEN',     // 在区间中查询 [BETWEEN, [1,9]] => BETWEEN 1 AND 9
+    NOT_BETWEEN  = 'NOT BETWEEN', // 不在区间中查询 [NOT_BETWEEN, [1,9]] => NOT BETWEEN 1 AND 9
+    IN           = 'IN',          // in数组 [IN, [1,3,6]] => IN (1, 3, 6)
+    NOT_IN       = 'NOT IN',      // 同IN取反
+    TIME_BETWEEN = 'TIME_BETWEEN' // 在时间区间
+}
