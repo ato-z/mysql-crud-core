13b3e25fc12d53c836503e1f7162b30ca4214fe4
ato-z <523333404@qq.com>
2023-01-04 15:46:02 +0800

    v1.0.9

--- src/action/Select.ts
+++ src/action/Select.ts
@@ -2,8 +2,9 @@ import { withWhere } from "../core/WithWhere"
 import { SelectOption } from "./Option"
 
 export const Select = <T>(tableName: string, option: SelectOption<T> = {}) => {
-    const { field, where, order, limit } = option
-    const sqlSelectFrom = [`SELECT ${field?.map(f => `\`${f}\``) ?? '*'} FROM \`${tableName}\``]
+    const { field = '*', where, order, limit } = option
+    const fieldProp = field instanceof Array ? field?.map(f => `\`${f}\``) : field
+    const sqlSelectFrom = [`SELECT ${fieldProp} FROM \`${tableName}\``]
     /** 组合where条件 */
     if (where !== undefined) { sqlSelectFrom.push(withWhere(where.and, where.or, option.join)) }
     /** 排序 */
